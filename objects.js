08eb80488b04bbdf49c5964bb777b1a060b3f342
sofiyan mohammed <sofiyanmohammedsuf@gmail.com>
2020-01-30 16:03:38 +0530

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,15 @@
+var obj1 = {
+    a: 1,
+    b: 2
+}
+var obj2 = {
+    a: 1,
+    b: 2
+}
+if (obj1.a == obj2.a && obj1.b == obj2.b) {
+    //return true;
+    console.log(true)
+} else {
+    //return false;
+    console.log(false)
+}
